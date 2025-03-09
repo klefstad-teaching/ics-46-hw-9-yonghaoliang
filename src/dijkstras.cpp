3cbf35b8536d778364eee7856806bce9047116b7
yonghaoliang <yonghal2@uci.edu>
2025-03-09 07:43:14 +0000

    hw00009

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -36,16 +36,17 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
     vector<int> path;
-    if (distances[destination] == INF){
+    if (distances[destination] == INF) {
         return path;
     }
-    for (int at = destination; at != -1; at = previous[at]){
+    for (int at = destination; at != -1; at = previous[at]) {
         path.push_back(at);
     }
     reverse(path.begin(), path.end());
     return path;
+}
 
-void print_path(const vector<int>& path, int total){
+void print_path(const vector<int>& path, int total) {
     if (path.empty()) {
         cout << "No path found." << endl;
         return;
